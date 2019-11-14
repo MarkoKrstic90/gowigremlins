f3adf2f4481f7aeefa62b7ceeb7c17627d16e83a
Marko Krstic <markodmm@gmail.com>
2019-11-15 00:40:00 +0100

    Basic example added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Graph } from "react-d3-graph";
+
+// graph payload (with minimalist structure)
+const data = {
+  nodes: [{ id: "Global" }, { id: "Europe" }, { id: "North America" }, {id: "Serbia"}],
+  links: [{ source: "Global", target: "Europe" }, { source: "Global", target: "North America" }, {source:"Europe", target:"Serbia"}],
+};
+
+// the graph configuration, you only need to pass down properties
+// that you want to override, otherwise default ones will be used
+const myConfig = {
+  nodeHighlightBehavior: true,
+  node: {
+      color: "lightgreen",
+      size: 120,
+      highlightStrokeColor: "blue",
+  },
+  link: {
+      highlightColor: "lightblue",
+  },
+};
+
+// graph event callbacks
+const onClickGraph = function() {
+  //window.alert(`Clicked the graph background`);
+};
+
+const onClickNode = function(nodeId) {
+  //window.alert(`Clicked node ${nodeId}`);
+};
+
+const onDoubleClickNode = function(nodeId) {
+  //window.alert(`Double clicked node ${nodeId}`);
+};
+
+const onRightClickNode = function(event, nodeId) {
+  //window.alert(`Right clicked node ${nodeId}`);
+};
+
+const onMouseOverNode = function(nodeId) {
+  //window.alert(`Mouse over node ${nodeId}`);
+};
+
+const onMouseOutNode = function(nodeId) {
+  //window.alert(`Mouse out node ${nodeId}`);
+};
+
+const onClickLink = function(source, target) {
+  //window.alert(`Clicked link between ${source} and ${target}`);
+};
+
+const onRightClickLink = function(event, source, target) {
+  //window.alert(`Right clicked link between ${source} and ${target}`);
+};
+
+const onMouseOverLink = function(source, target) {
+  //window.alert(`Mouse over in link between ${source} and ${target}`);
+};
+
+const onMouseOutLink = function(source, target) {
+  //window.alert(`Mouse out link between ${source} and ${target}`);
+};
+
+const onNodePositionChange = function(nodeId, x, y) {
+  //window.alert(`Node ${nodeId} is moved to new position. New position is x= ${x} y= ${y}`);
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Graph
+    id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+    data={data}
+    config={myConfig}
+    onClickNode={onClickNode}
+    onRightClickNode={onRightClickNode}
+    onClickGraph={onClickGraph}
+    onClickLink={onClickLink}
+    onRightClickLink={onRightClickLink}
+    onMouseOverNode={onMouseOverNode}
+    onMouseOutNode={onMouseOutNode}
+    onMouseOverLink={onMouseOverLink}
+    onMouseOutLink={onMouseOutLink}
+    onNodePositionChange={onNodePositionChange}
+/>
   );
 }
 
